8cf327403ce0e83ff985e9d4607cb342a16bd509
yunzhao100 <147044420+yunzhao100@users.noreply.github.com>
2026-09-12 14:11:43 +0100

    Add: house-robber-ii - submission-9

---
+++ Data Structures & Algorithms/house-robber-ii/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n<=2: return max(nums)
+        dic1={n-2:nums[n-2],n-3:max(nums[n-3],nums[n-2])}
+        def dp1(i):
+            if i in dic1: return dic1[i]
+            dic1[i] = max(nums[i]+dp1(i+2),dp1(i+1))
+            return dic1[i]
+        
+        dic2={n-1:nums[n-1],n-2:max(nums[n-2],nums[n-1])}
+        def dp2(i):
+            if i in dic2: return dic2[i]
+            dic2[i] = max(nums[i]+dp2(i+2),dp2(i+1))
+            return dic2[i]
+        return max(dp1(0),dp2(1))
